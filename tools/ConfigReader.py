17e10fa7e8d0423593f540ffe49376c618db77cb
Alexander Veltman <alexveltman@protonmail.com>
2021-03-22 12:16:32 +0200

    Added docs and refactored

--- tools/ConfigReader.py
+++ tools/ConfigReader.py
@@ -5,6 +5,7 @@ from numpy.linalg import inv
 
 
 class ConfigReader:
+    """ Reads JSON configuration file"""
     def __init__(self, filename: str):
         self.filename = filename
         coefficients = []
@@ -26,7 +27,7 @@ class ConfigReader:
             self.n_burnin = self.params["config"]["fit"]["n_burnin"]
             self.n_total = self.params["config"]["fit"]["n_total"]
             self.cov = self.params["config"]["data"]["covariance_matrix"]
-            self.icov = inv(self.params["config"]["data"]["covariance_matrix"])
+            self.icov = inv(self.cov)
             self.x_vals = self.params["config"]["data"]["bins"]
             self.samples = np.asarray(self.params["config"]["model"]["samples"])
             if self.params["config"]["model"]["input"] == "numpy":
@@ -46,9 +47,9 @@ class ConfigReader:
 
         self.tex_labels = ["$" + c + "$" for c in self.coefficients]
         x_vals = np.zeros(len(self.x_vals) - 1)
-        for x_val in range(0, len(self.params["config"]["data"]["bins"]) - 1):
+        for x_val in range(0, len(self.bins) - 1):
             x_vals[x_val] = (
-                self.params["config"]["data"]["bins"][x_val]
-                + self.params["config"]["data"]["bins"][x_val + 1]
+                self.bins[x_val]
+                + self.bins[x_val + 1]
             ) / 2.0
         self.x_vals = x_vals
--- tools/PredictionBuilder.py
+++ tools/PredictionBuilder.py
@@ -4,23 +4,27 @@ from sklearn.linear_model import LinearRegression
 
 from typing import List
 
-def triangularNumber(n: int) -> int:
+
+def triangular_number(n: int) -> int:
     return int(n * (n + 1) / 2)
+
 
 class PredictionBuilder:
     """
-    Constructs Morphing Model for the wilson coefficient of each operator
+    Constructs Linear Regression Model for the wilson coefficient of each operator for some observable
     """
-    
+
     def __init__(self, nOps: int, samples: List[float], preds: List[List[float]]):
-        """Constuctor for PredictionBuilder"""
-        
+        """ Constructor for PredictionBuilder """
+
         self.nOps = nOps
         self.nSamples = int((nOps + 1) * (nOps + 2) / 2)
-        self.model = self.initRM(nOps, samples, preds)
-        
-    def initRM(self, nOps: int, samples: List[float], preds: List[List[float]]) -> LinearRegression:
-        """Initialise morphing model using samples with predicted values"""
+        self.model = self.build_regression_model(nOps, samples, preds)
+
+    def build_regression_model(
+        self, nOps: int, samples: List[float], preds: List[List[float]]
+    ) -> LinearRegression:
+        """ Initialise morphing model using samples with predicted values """
         if len(preds) < self.nSamples:
             raise TypeError(
                 "morphing with "
@@ -33,49 +37,45 @@ class PredictionBuilder:
             )
 
         # convert to vector of coefficient factors to linearise the morphing
-        cInputAr = self.makeCoeff(samples)
+        cInputAr = self.make_coefficients(samples)
 
         # define model
         model = LinearRegression()
 
         # fit model
         model.fit(cInputAr, preds)
-        
+
         return model
 
-    
-    def makeCoeff(self, ci):
+    def make_coefficients(self, ci: List[float]) -> np.ndarray:
         X = np.array([])
         num_rows = np.shape(ci)[0]
         for row in ci:
+            # Account for quadratic self term
             X = np.append(X, row ** 2)
+
+            # Account for quadratic cross term
             combs = itertools.combinations(list(row), 2)
-            
             for comb in combs:
-                X = np.append(X, comb[0] * comb[1])
-
-        X = X.reshape(num_rows, triangularNumber(len(ci[0])))
-
+                X = np.append(X, comb[0] * comb[1])        
+        X = X.reshape(num_rows, triangular_number(len(ci[0])))
         return X
 
-    def makeCoeffPoint(self, ci):
+    def makeCoeffPoint(self, ci: np.ndarray) -> np.ndarray:
         X = np.array([])
         X = np.append(X, ci ** 2)
         combs = itertools.combinations(list(ci), 2)
         for comb in combs:
             X = np.append(X, comb[0] * comb[1])
 
-        X = X.reshape(1, triangularNumber(len(ci)))
+        X = X.reshape(1, triangular_number(len(ci)))
         return X
 
-
-
-    def makeRMPred(self, c):
-
+    def make_prediction(self, c: np.ndarray) -> np.ndarray :
+        """ Produce the predicted observable for a set of coefficients (excluding SM coefficient) """
         c = np.append(1.0, c)
         cInputAr = self.makeCoeffPoint(c)
         pred = self.model.predict(cInputAr)
-
         return pred[0]
 
     # TODO: Not in use. Figure out if it can be deleted
